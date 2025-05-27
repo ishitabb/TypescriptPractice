103126c3e43238adbfe36f4935af009e1bd8eecb
Ishita.Joshi <Ishita.Joshi@valuespaces.com>
2025-05-27 21:34:56 +0200

    Test.ts - Learned creating typed function and reduce function to return sum of elements of an array

--- Test.ts
+++ Test.ts
@@ -1,1 +1,7 @@
-console.log("Practice Programs")
+//A function that takes an array of numbers and returns their sum. 
+// Use TypeScript to enforce that the input is an array of numbers.
+function sumOfArray(a : number[]) : number {
+    return a.reduce((sum, num) => sum + num,0 )
+}
+
+console.log(sumOfArray([3,5]))
